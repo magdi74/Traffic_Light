c90c2905e67f330ed90a1c824c8c3d332d5c9f32
magdi74 <70348804+magdi74@users.noreply.github.com>
2021-11-04 00:49:27 +0200

    adding 3 functions

--- DIO.h
+++ DIO.h
@@ -1,3 +1,5 @@
+# include "type.h"
+
 # define IN 0
 # define OUT 1
 # define PORTA 0
@@ -6,3 +8,7 @@
 # define PORTD 3
 # define PORTE 4
 # define PORTF 5
+
+void DIO_Init (int8 port, int8 pin, int8 dir);
+void DIO_WritePort(int8 port,int8 value);
+void DIO_WritePin (int8 port, int8 pin, int8 value);
