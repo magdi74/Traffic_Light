2026992de8b45a50d0be633f31356b9f40fc32a3
magdi74 <70348804+magdi74@users.noreply.github.com>
2021-11-04 00:50:45 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,34 @@
-#include <stdint.h>
-#include "tm4c123gh6pm.h"
-#include "bitwise_operation.h"
+# include "DIO.h"
+# include <stdint.h>
+# include "tm4c123gh6pm.h"
+# include "bitwise_operation.h"
 
-int main()
-{
-  set_bit(SYSCTL_RCGCGPIO_R,5)
-  //SYSCTL_RCGCGPIO_R |= 0x00000020;
-
-  while((SYSCTL_PRGPIO_R&0x00000020) == 0){};
+int main(){
   
-  GPIO_PORTF_LOCK_R = 0x4C4F434B;
-  GPIO_PORTF_CR_R = 0x1F;
-  GPIO_PORTF_DIR_R = 0x0E;
-  GPIO_PORTF_PUR_R = 0x11;
-  GPIO_PORTF_DEN_R = 0x1F;
-  GPIO_PORTF_DATA_R = 0x0E;
+  DIO_Init(PORTF,0,IN);
+  DIO_Init(PORTF,1,OUT);
+  DIO_Init(PORTF,2,OUT);
+  DIO_Init(PORTF,3,OUT);
+  DIO_Init(PORTF,4,IN);
+  
+  while(1){
+    int volatile counter = 0 ;
 
-  return 0;
+    while(counter < 1000000){
+    DIO_WritePort(PORTF,0x02);  //red color
+    ++counter;
+  }
+  
+  counter = 0 ;
+  while(counter < 1000000){
+    DIO_WritePort(PORTF,0x04);  //blue color
+    ++counter;
+  }
+  
+  counter = 0 ;
+  while(counter < 1000000){
+    DIO_WritePort(PORTF,0x08);  //Green color
+    ++counter;
+  }
+}
 }
